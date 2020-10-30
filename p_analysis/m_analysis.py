c5d6c731eae7b67dc8349a7d5d09dddb7b5e4bd9
Josue Rivera <jzjosuerivera71@gmail.com>
2020-10-30 17:21:04 +0100

    third-commit

--- p_analysis/m_analysis.py
+++ p_analysis/m_analysis.py
@@ -1,23 +1,6 @@
 
 #https://www.kaggle.com/dferhadi/covid-19-predictions-growth-factor-and-calculus/
 
-
-
-def smoother(inputdata,w,imax):
-    data = 1.0*inputdata
-    data = data.replace(np.nan,1)
-    data = data.replace(np.inf,1)
-    #print(data)
-    smoothed = 1.0*data
-    normalization = 1
-    for i in range(-imax,imax+1):
-        if i==0:
-            continue
-        smoothed += (w**abs(i))*data.shift(i,axis=0)
-        normalization += w**abs(i)
-    smoothed /= normalization
-    return smoothed
-    
 # function to compute growth factor
 def growth_factor(confirmed):
     confirmed_iminus1 = confirmed.shift(1, axis=0)
@@ -31,61 +14,4 @@ def growth_ratio(confirmed):
     return (confirmed/confirmed_iminus1)
     
 
-# This is a function which plots (for in input country) the active, confirmed, and recovered cases, deaths, and the growth factor.
-def plot_country_active_confirmed_recovered_growth_metrics(country):
-    
-    # Plots Active, Confirmed, and Recovered Cases. Also plots deaths.
-    country_data = global_data[global_data['Country/Region']==country]
-    table = country_data.drop(['SNo','Province/State', 'Last Update'], axis=1)
-    table['ActiveCases'] = table['Confirmed'] - table['Recovered'] - table['Deaths']
-    table2 = pd.pivot_table(table, values=['ActiveCases','Confirmed', 'Recovered','Deaths'], index=['ObservationDate'], aggfunc=np.sum)
-    table3 = table2.drop(['Deaths'], axis=1)
-   
-    # Growth Factor
-    w = 0.5
-    table2['GrowthFactor'] = growth_factor(table2['Confirmed'])
-    table2['GrowthFactor'] = smoother(table2['GrowthFactor'],w,5)
 
-    # 2nd Derivative
-    table2['2nd_Derivative'] = np.gradient(np.gradient(table2['Confirmed'])) #2nd derivative
-    table2['2nd_Derivative'] = smoother(table2['2nd_Derivative'],w,7)
-
-
-    #Plot confirmed[i]/confirmed[i-1], this is called the growth ratio
-    table2['GrowthRatio'] = growth_ratio(table2['Confirmed'])
-    table2['GrowthRatio'] = smoother(table2['GrowthRatio'],w,5)
-    
-    #Plot the growth rate, we will define this as k in the logistic function presented at the beginning of this notebook.
-    table2['GrowthRate']=np.gradient(np.log(table2['Confirmed']))
-    table2['GrowthRate'] = smoother(table2['GrowthRate'],0.5,3)
-    
-    # horizontal line at growth rate 1.0 for reference
-    x_coordinates = [1, 100]
-    y_coordinates = [1, 1]
-    #plots
-    table2['Deaths'].plot(title='Deaths')
-    plt.show()
-    table3.plot() 
-    plt.show()
-    table2['GrowthFactor'].plot(title='Growth Factor')
-    plt.plot(x_coordinates, y_coordinates) 
-    plt.show()
-    table2['2nd_Derivative'].plot(title='2nd_Derivative')
-    plt.show()
-    table2['GrowthRatio'].plot(title='Growth Ratio')
-    plt.plot(x_coordinates, y_coordinates)
-    plt.show()
-    table2['GrowthRate'].plot(title='Growth Rate')
-    plt.show()
-
-##########################################################################
-
- # import plotly.express as px
-   # table3 = table3.melt(id_vars="ObservationDate", value_vars=['ActiveCases','Confirmed','Recovered'],
-   #              var_name='case', value_name='count')
-   # table3.reindex()
-   # fig = px.area(table3, x="ObservationDate", y="count", color='case',
-   #              title='Confirmed Cases', color_discrete_sequence = ['cyan', 'red', 'orange'])
-   # fig.show()
-
-    return             
