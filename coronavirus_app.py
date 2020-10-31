0254c345929b6e41ee737ebfdb1de138cb5fe8ef
Josue Rivera <jzjosuerivera71@gmail.com>
2020-10-31 03:18:58 +0100

    proyect-finished

--- coronavirus_app.py
+++ coronavirus_app.py
@@ -29,49 +29,80 @@ from p_analysis import m_analysis as an
 
 #INTRODUCTION
 
-st.title('Introduction')
-st.text('This is some text.')
+st.title('LSTM neural network for timeseries evolution of Coronavirus.')
 
-st.markdown('Streamlit is **_really_ cool**.')
 
-st.header('This is a header')
-st.subheader('This is a subheader')
+st.header('Introduction.')
+
+st.subheader('Problem description:')
+st.write('The main idea of the project is to train several recurrent neural networks to be able to predict the evolution of Covid19 as accurately as possible. This will help us to make better decisions about population control.')
+
+
+st.subheader('Original Model.')
+st.write('From this differntial equation:')
+st.latex( r'''  \partial_t f = f(t)(N - f(t))  ''')     
+st.write('we can deduce the solution that we use to do the sigmoidal regretion. This regretion is the most used for this type of analisis')     
+st.latex(r''' f(t) = \frac {N}{1 + e^{-k(t - t_0)}} ''')
+st.write('This model is very useful but there is some limitations, especialy in the fluctuations of the growth')
 
 ########################################################
 
 # METODOLOGY
 
-st.title('Metodology')
-st.text('This is some text.')
-st.write("Josue")
+st.header('Metodology.')
 
-###########
 
-# Por si acaso
-
-#df = pd.DataFrame(
-#    np.random.randn(50, 20),
-#    columns=('col %d' % i for i in range(20)))
-
-#st.dataframe(df)  # Same as st.write(df)
-
-#############
-
-st.latex(r'''
-     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-     \sum_{k=0}^{n-1} ar^k =
-     a \left(\frac{1-r^{n}}{1-r}\right)
-     ''')
-     
+st.subheader('LSTM neural Network Model')
+st.write('This option is better to predict the evolution of the disease because the prediction is adjusted according to the previous data. Additionally this model is easy to train.')
 #image = ??????
 #st.image(image, caption=None, width=None, use_column_width=False, clamp=False, channels='RGB', output_format='auto', **kwargs)
+# st.header("A cat")
+st.image("data/LSTM.png", use_column_width=True)
 
-st.latex(r'''
-     a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
-     \sum_{k=0}^{n-1} ar^k =
-     a \left(\frac{1-r^{n}}{1-r}\right)
-     ''')
+st.subheader('Pipeline dependencies:')
+st.write('- numpy')
+st.write('- pandas')
+st.write('- pytorch')
+st.write('- matplotlib')
+st.write('- datetime')
+st.write('- scipy')
+st.write('- sklearn')
+st.write('- streamlit')
+
+st.subheader('Pipeline componets:') 
+st.write('- 3 modules (preprocesing, analisis, models)')
+st.write('- 1 LSTM class')
+st.write('- 2 train neural networks')
+st.write('- 3 analisis functions')
+st.write('- 4 preprocesing functions')
+
+st.subheader('Training Scheme:')
+st.write('The original dataset grouped the accumulated cases and deaths by county. To do the training, the totals had to be added by date.') 
+st.graphviz_chart('''
+    digraph {
+        dataset -> totals
+        totals -> cases
+        totals -> deaths
+        cases -> training
+        deaths -> training
+        training -> models
+    }
+''')       
      
+
+st.subheader('Data transformation:')
+st.write('To make the predictions, the same had to be done, but for each state.The result was two datasets which are used in the demonstration.')  
+st.graphviz_chart('''
+    digraph {
+        original -> states_data
+        states_data -> LSTM
+        LSTM -> Predictions
+    }
+''')     
+
+       
+st.subheader('Pytorch LSTM class ')
+st.write("This is the most importand part of the pipeline.")
 
 code = '''class LSTM(nn.Module):
     		def __init__(self, input_dim, output_dim, hidden_layer_size, num_layers):
@@ -98,29 +129,11 @@ code = '''class LSTM(nn.Module):
      
 st.code(code, language='python')
 
-st.graphviz_chart('''
-    digraph {
-        run -> intr
-        intr -> runbl
-        runbl -> run
-        run -> kernel
-        kernel -> zombie
-        kernel -> sleep
-        kernel -> runmem
-        sleep -> swap
-        swap -> runswap
-        runswap -> new
-        runswap -> runmem
-        new -> runmem
-        sleep -> runmem
-    }
-''')
-
 
 ##########################################################
 
 #DEMO
-st.title('USA Coronavirus evolution')
+st.header('Demo of the LSTM pipeline')
 
 # Data
 cases = pd.read_csv('data/acumulated_cases.csv')
@@ -179,40 +192,50 @@ actual_deaths = pp.Actual(deaths_case)
 
 # ANALISIS
 
-#x_data = range(len(country_data.index))
-#y_data = country_data['Confirmed']
+# Sigmoidal function
 
-x_data = range(len(state_case))
-y_data = state_case
+data_cases = cases[option_2]
+data_deaths = deaths[option_2]
 
 def log_curve(x, k, x_0, ymax):
     return ymax / (1 + np.exp(-k*(x-x_0)))
     
+def selection_x(option_1):
+	if option_1 == 'Cases':
+		x_data = range(len(cases.index))
+		return x_data
+	else:
+		x_data = range(len(deaths.index))
+		return x_data
+    
+def selection_y(option_1,data_cases,data_deaths):
+	if option_1 == 'Cases':
+		#x_data = range(len(cases.index))
+		y_data = data_cases
+		return y_data
+	else:
+		#x_data = range(len(cases.index))
+		y_data = data_deaths
+		return y_data
+			
 # Fit the curve
+x_data = selection_x(option_1)
+y_data = selection_y(option_1,data_cases,data_deaths)
 popt, pcov = curve_fit(log_curve, x_data, y_data, bounds=([0,0,0],np.inf), maxfev=50000)
 estimated_k, estimated_x_0, ymax= popt
-
-
 
 # Plot the fitted curve
 k = estimated_k
 x_0 = estimated_x_0
-y_fitted = log_curve(cases_size, k, x_0, ymax)
-#print(k, x_0, ymax)
-#print(y_fitted)
-#y_data.tail()
+y_fitted = log_curve(range(0,230), k, x_0, ymax)
 
 
-# Plot everything for illustration
-#fig = plt.figure()
-#ax = fig.add_subplot(111)
-#ax.plot(range(0,350), y_fitted, '--', label='fitted')
-#ax.plot(x_data, y_data, 'o', label='Confirmed Data')
-
-
+# Calculations
 
 #growth = an.growth_factor(cases_predict)
 #an.growth_ratio(confirmed)
+
+
 ###################################################################
 
 # PLOT
@@ -226,6 +249,7 @@ if  option_1 == 'Cases':
 	ax[0].plot(actual_cases, c='b', label='Actual Data')
 	ax[0].axvline(x=cases_size, linestyle='--')
 	ax[0].legend(loc='upper left')
+	#
 	ax[1].set_title('Sigmoidal')
 	ax[1].plot(y_fitted, c='r', label='Sigmoidal acumulated cases predictions')
 	ax[1].plot(actual_cases, c='b', label='Actual Data')
@@ -238,20 +262,14 @@ elif option_1 == 'Deaths':
 	ax[0].plot(actual_deaths, c='b', label='Actual Data')
 	ax[0].axvline(x=deaths_size, linestyle='--')
 	ax[0].legend(loc='upper left')
-	#ax[1].set_title('Deaths')
-	#ax[1].plot(deaths_predict, c='r', label='LSTM acumulated deaths predictions')
-	#ax[1].plot(actual_deaths, c='b', label='Actual Data')
-	#ax[1].axvline(x=deaths_size, linestyle='--')
-	#ax[1].legend(loc='upper left')
-
+	#
+	ax[1].set_title('Sigmoidal')
+	ax[1].plot(y_fitted, c='r', label='Sigmoidal acumulated cases predictions')
+	ax[1].plot(actual_cases, c='b', label='Actual Data')
+	ax[1].axvline(x=deaths_size, linestyle='--')
+	ax[1].legend(loc='upper left')
 
 ######################################################
-
-#ax[0][2].set_title('Growth Factor')
-#ax[0][2].plot(growth, c='r', label='Growth Factor')
-#ax[0][2].plot(actual_deaths, c='b', label='Actual Data')
-#ax[0][2].axvline(x=cases_size, linestyle='--')
-#ax[0][2].legend(loc='upper left')
 
 st.pyplot(fig)
 
